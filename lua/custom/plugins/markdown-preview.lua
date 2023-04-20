0dfefee2490776676004be36ef1f68bb0e7f05b0
Alex Lee <alexlee@fastmail.com>
2023-04-19 21:22:28 -0500

    Add markdown-preview

---
+++ lua/custom/plugins/markdown-preview.lua
@@ -0,0 +1,10 @@
+-- markdown-preview
+-- https://github.com/iamcco/markdown-preview.nvim
+
+return {
+  'iamcco/markdown-preview.nvim',
+  ft = 'markdown',
+  build = function()
+    vim.fn['mkdp#util#install']()
+  end,
+}
--- lua/custom/plugins/vim-test.lua
+++ lua/custom/plugins/vim-test.lua
@@ -4,7 +4,6 @@ return {
   'vim-test/vim-test',
   lazy = false,
   config = function()
-    local keymap_opts = { noremap = true, silent = true }
     vim.keymap.set('n', '<leader>tn', ':TestNearest<CR>', { desc = '[T]est [N]earest' })
     vim.keymap.set('n', '<leader>tf', ':TestFile<CR>', { desc = '[T]est [F]ile' })
     vim.keymap.set('n', '<leader>tl', ':TestLast<CR>', { desc = '[T]est [L]last' })
