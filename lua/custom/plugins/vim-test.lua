81df3aa2620f3c291e85e72c73b8da600e6b4e68
Alex Lee <alexlee@fastmail.com>
2024-09-03 19:17:54 -0500

    Specify pytest

--- lua/custom/plugins/vim-test.lua
+++ lua/custom/plugins/vim-test.lua
@@ -7,5 +7,7 @@ return {
     vim.keymap.set('n', '<leader>tn', ':TestNearest<CR>', { desc = '[T]est [N]earest' })
     vim.keymap.set('n', '<leader>tf', ':TestFile<CR>', { desc = '[T]est [F]ile' })
     vim.keymap.set('n', '<leader>tl', ':TestLast<CR>', { desc = '[T]est [L]last' })
+
+    vim.g['test#python#runner'] = 'pytest'
   end,
 }
