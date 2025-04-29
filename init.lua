80c65f87e2cc7142a78cd9a667525e8c918362e5
Alex Lee <alexlee@fastmail.com>
2025-04-29 15:17:21 -0500

    Enable toggling of conform format

--- init.lua
+++ init.lua
@@ -809,6 +809,11 @@ require('lazy').setup({
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
+        -- Skip if disabled. See after/plugin/conform-nvim.lua
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
