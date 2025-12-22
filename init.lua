177ab9d80bf2ae3a69fb4799ac1356de35eb2008
Alex Lee <alexlee@fastmail.com>
2025-12-22 12:54:25 -0600

    Show percentage in status line

--- init.lua
+++ init.lua
@@ -1012,7 +1012,7 @@ require('lazy').setup({
       -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
-        return '%2l:%-2v'
+        return '%p%% %3l:%-2v'
       end
 
       -- ... and there is more!
